0156bdc4b1389605ac0255f9913e12aa9e14dc1e
Boppana Naga Venkata Mahidhar Babu <fswd2004@gmail.com>
2025-10-22 21:08:43 +0530

    add new floder to repo

---
+++ FORM-VALIDATER/script.js
@@ -0,0 +1,99 @@
+const form = document.getElementById("registration-form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const isRequiredValid = checkRequired([
+    username,
+    email,
+    password,
+    confirmPassword,
+  ]);
+
+  let isFormValid = isRequiredValid;
+
+  if (isRequiredValid) {
+    const isUsernameValid = checkLength(username, 3, 15);
+    const isEmailValid = checkEmail(email);
+    const isPasswordValid = checkLength(password, 6, 25);
+    const isPasswordMatch = checkPasswordMatch(password, confirmPassword);
+
+    isFormValid =
+      isUsernameValid && isEmailValid && isPasswordValid && isPasswordMatch;
+  }
+
+  if (isFormValid) {
+    alert("Registration Successful!");
+    form.reset();
+    document.querySelectorAll(".form-group").forEach((group) => {
+      group.className = "form-group";
+    });
+  }
+});
+
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+    return false;
+  }
+  return true;
+}
+
+function checkEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(email.value.trim())) {
+    showSuccess(email);
+    return true;
+  } else {
+    showError(email, `${formatFieldName(email)}is not valid`);
+    return false;
+  }
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${formatFieldName(input)} must be at least ${min}`);
+    return false;
+  } else if (input.value.length > max) {
+    showError(input, `${formatFieldName(input)} must be less than ${max}`);
+    return false;
+  } else {
+    showSuccess(input);
+    return true;
+  }
+}
+
+function checkRequired(inputArray) {
+  let isValid = true;
+
+  inputArray.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${formatFieldName(input)} is required`);
+      isValid = false;
+    } else {
+      showSuccess(input);
+    }
+  });
+
+  return isValid;
+}
+
+function formatFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function showError(input, message) {
+  const fromGroup = input.parentElement;
+  fromGroup.className = "form-group error";
+  const small = fromGroup.querySelector("small");
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const fromGroup = input.parentElement;
+  fromGroup.className = "form-group success";
+}
